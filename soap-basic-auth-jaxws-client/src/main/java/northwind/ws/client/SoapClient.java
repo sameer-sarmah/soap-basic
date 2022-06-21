d01e85149b255627f4f6d7537a249990d77ea82c
Sameer Sarmah <s.sarmah@sap.com>
2022-06-21 11:59:56 +0530

    JAX-WS SOAP client with Dispatch

--- soap-basic-auth-jaxws-client/src/main/java/northwind/ws/client/SoapClient.java
+++ soap-basic-auth-jaxws-client/src/main/java/northwind/ws/client/SoapClient.java
@@ -1,12 +1,23 @@
 package northwind.ws.client;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Map;
 import java.util.Optional;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.namespace.QName;
 import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Dispatch;
 import javax.xml.ws.Service;
+import javax.xml.ws.soap.SOAPBinding;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -29,6 +40,13 @@ public class SoapClient {
 	public Optional<Customer> getCustomer(String customerId) {
 		GetCustomerRequest customerRequest = new GetCustomerRequest();
 		customerRequest.setCustomerID(customerId);
+		//GetCustomerResponse customerResponse = getCustomerResponse(customerRequest);
+		GetCustomerResponse customerResponse = getCustomerResponseWithDispatcher(customerRequest);
+		return Optional.of(customerResponse.getCustomer());
+
+	}
+	
+	private GetCustomerResponse getCustomerResponse(GetCustomerRequest customerRequest) {
 		URL wsdlUrl = this.getClass().getClassLoader().getResource("customers.wsdl");
 		QName serviceName = new QName("http://domain.northwind/customer","CustomersPortService");
 		Service customerService = Service.create(wsdlUrl, serviceName);
@@ -37,7 +55,37 @@ public class SoapClient {
 		prov.getRequestContext().put(BindingProvider.USERNAME_PROPERTY,username);
 		prov.getRequestContext().put(BindingProvider.PASSWORD_PROPERTY,password);
 		GetCustomerResponse customerResponse = customerInterface.getCustomer(customerRequest);
-		return Optional.of(customerResponse.getCustomer());
+		return customerResponse;
+	}
+	
+	private GetCustomerResponse getCustomerResponseWithDispatcher(GetCustomerRequest customerRequest) {
+		URL wsdlUrl = this.getClass().getClassLoader().getResource("customers.wsdl");
+		QName serviceName = new QName("http://domain.northwind/customer","CustomersPortService");
+		Service customerService = Service.create(wsdlUrl, serviceName);
+		QName portName = new QName("http://domain.northwind/customer","CustomersPortSoap11");
+		customerService.addPort(portName, SOAPBinding.SOAP11HTTP_BINDING, "http://localhost:8888/service/customer");
+	    JAXBContext jaxbContext =  getJAXBContext();
+
+	    Dispatch<Object> dispatch = customerService.createDispatch(portName, jaxbContext, Service.Mode.PAYLOAD);
+	    Map<String, Object> requestContext = dispatch.getRequestContext();
+	    requestContext.put(Dispatch.PASSWORD_PROPERTY, password);
+	    requestContext.put(Dispatch.USERNAME_PROPERTY, username);
+	    
+		GetCustomerResponse customerResponse = (GetCustomerResponse)dispatch.invoke(customerRequest);
+		return customerResponse;
+	}
+	
+    private JAXBContext getJAXBContext() {
+        JAXBContext jaxBcontext = null;
+		try {
+			jaxBcontext = JAXBContext.newInstance("northwind.domain.customer");
+	        Marshaller marshaller= jaxBcontext.createMarshaller();
+	        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+	        marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);	        
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} 
+		return jaxBcontext;
 
 	}
 }
